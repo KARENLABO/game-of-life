e3b3eb77f46f51cff1708dad416cb1f8c023077b
karenlabo <karen.bussines@hotmail.com>
2022-09-14 12:33:29 -0500

    Create a header div to incluide rules, and add styles for that class

--- Karen/game-of-life/src/components/Table.jsx
+++ Karen/game-of-life/src/components/Table.jsx
@@ -5,7 +5,7 @@ import {nextState, copy} from './helperFunctions'
 
 
 function Table() {
-  const spaces = 60;
+  const spaces = 50;
   const [cells, setCells] = useState(new Array(spaces).fill('').map(() => new Array(spaces).fill(false)));
   const [runProgram, setRunProgram] = useState(false);
 
@@ -43,14 +43,25 @@ function Table() {
     }, 300);
 
     return(
-      <div>
-        <div className='butons-menu'>
-          <button onClick={onClickStart} disabled={runProgram}>
-            Start</button>
-          <button onClick={onClickPause}disabled={!runProgram}>Pause</button>
-          <button onClick={onClickCustom}>Random Values</button>
-          <button onClick={onClickClear}>Clear Matrix</button>
+      <div className='game-container'>
+        <h1>THE GAME OF LIFE</h1>
+        <div className='header'>
+          <div className='rules'>
+            <h4> Rules:</h4>
+            <li> If the cell is alive, it will stay alive if it has 2 or 3 neighbors alive.</li>
+            <li> If the cell is dead, then it comes to life only in case it has 3 neighbors alive.</li>
+          </div>
+
+          <div className='menus-buttons'>
+            <button onClick={onClickStart} disabled={runProgram}>
+              Start</button>
+            <button onClick={onClickPause} disabled={!runProgram}>Pause</button>
+            <button onClick={onClickCustom}>Random Values</button>
+            <button onClick={onClickClear}>Clear Matrix</button>
+          </div>
         </div>
+      
+
         <div>
           <table className='table-container' cellPadding={0} cellSpacing={0}> 
             <tbody>
