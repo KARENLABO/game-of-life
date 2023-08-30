1b44d3b4f6cf5530b66880b7353da0ae75ea6021
karen <karen.bussines@hotmail.com>
2023-08-29 19:06:36 -0600

    Refactor: move the top outside of table

---
+++ src/components/Table/Components/Cell/Cell.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Cell = ({ x, y, isActive, onClickCell }) => {
+  return (
+    <td
+      id={x + y}
+      style={{
+        backgroundColor: isActive ? "#6420ff" : "white",
+      }}
+      onClick={() => onClickCell(x, y)}
+    ></td>
+  );
+};
+
+export default Cell;
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import Cell from "./Components/Cell/Cell";
+import "./Table.scss";
+import Header from "../Header";
+import { nextState, copy } from "../helperFunctions";
+
+function Table() {
+  const spaces = 50;
+  const [cells, setCells] = useState(
+    new Array(spaces).fill("").map(() => new Array(spaces).fill(false))
+  );
+  const [runProgram, setRunProgram] = useState(false);
+
+  const onClickStart = () => {
+    setRunProgram(true);
+  };
+
+  const onClickCustom = () => {
+    if (!runProgram) {
+      setCells(
+        new Array(spaces)
+          .fill("")
+          .map(() => new Array(spaces).fill(Math.random() < 0.1))
+      );
+    }
+  };
+
+  const onClickClear = () => {
+    if (!runProgram) {
+      setCells(
+        new Array(spaces).fill("").map(() => new Array(spaces).fill(false))
+      );
+    }
+  };
+
+  const onClickPause = () => {
+    setRunProgram(false);
+  };
+
+  const onClickCell = (x, y) => {
+    if (!runProgram) {
+      const tempArray = copy(cells);
+      tempArray[x][y] = !tempArray[x][y];
+      setCells(tempArray);
+    }
+  };
+
+  setTimeout(() => {
+    if (runProgram) {
+      nextState(cells, setCells);
+    }
+  }, 300);
+
+  return (
+    <div className="game-container">
+      <Header
+        onClickPause={onClickPause}
+        onClickStart={onClickStart}
+        onClickClear={onClickClear}
+        onClickCustom={onClickCustom}
+        runProgram={runProgram}
+      />
+
+      <div>
+        <table className="table-container" cellPadding={0} cellSpacing={0}>
+          <tbody>
+            {cells.map((row, x) => {
+              return (
+                <tr key={x}>
+                  {row.map((column, y) => {
+                    return (
+                      <Cell
+                        key={y}
+                        onClickCell={onClickCell}
+                        isActive={column}
+                        x={x}
+                        y={y}
+                      />
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Table;
