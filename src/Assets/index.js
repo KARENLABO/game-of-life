583c1a3e128bbd1b2806174207167558eaa5894f
karen <karen.bussines@hotmail.com>
2023-08-29 19:05:28 -0600

    Feat: create new svg icons

---
+++ src/Assets/index.js
@@ -0,0 +1,8 @@
+export { default as Play } from "./Play.svg";
+export { default as Pause } from "./Pause.svg";
+export { default as ChevronRigth } from "./Chevron-right.svg";
+export { default as ChevronLeft } from "./Chevron-left.svg";
+export { default as Clear } from "./Clear.svg";
+export { default as ChevronRigthDisabled } from "./Chevron-right-disabled.svg";
+export { default as ChevronLeftDisabled } from "./Chevron-left-disabled.svg";
+export { default as ClearDisabled } from "./Clear-disabled.svg";
